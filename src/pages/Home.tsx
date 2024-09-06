9091023e9f93648a3bfb103d6184bff4cfd6eade
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-09-06 21:37:18 +0100

    home WIP

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,15 @@
 // Core
 import { useEffect, useState } from 'react';
 // Third party
-import { ChevronRight } from 'lucide-react';
+import { ArrowRightIcon, ChevronRight } from 'lucide-react';
 import Autoplay from "embla-carousel-autoplay";
 // Local
 import Container from '@/components/Container';
 import { Card, CardContent, CardHeader } from '@/components/ui/card';
 import { Carousel, CarouselApi, CarouselContent, CarouselItem } from '@/components/ui/carousel';
 import { Progress } from '@/components/ui/progress';
+import { Button } from '@/components/ui/button';
+import { Link } from 'react-router-dom';
 
 const skills = [
    { name: 'Coding', percentage: 95 },
@@ -69,13 +71,26 @@ const Home = () => {
                         <ChevronRight size={96} className="absolute -left-6" /> I am a <br />
                         Web Developer;<br />
 
-                        <span className="text-primary text-lg font-semibold mt-6">Frontend Specialist</span>
+                        <span className="text-primary text-lg font-semibold mt-6">
+                           Frontend Specialist
+                        </span>
                      </h1>
                   </CardContent>
                </Card>
             </div>
 
             <Card className="relative bg-primary">
+               <Link to="experience">
+                  <Button size="icon" className="absolute right-5 top-5 z-10">
+                     <ArrowRightIcon className="h-6 w-6" />
+                  </Button>
+               </Link>
+
+               <svg className="not-sr-only absolute -right-[5px] -top-[5px] z-0" width="157" height="155" viewBox="0 0 157 155" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M0 1H4.77243C34.1812 1 57.9999 24.9568 57.9999 54.3415C57.9999 79.2064 78.1592 99.455 103 99.455C131.745 99.455 155.145 122.338 155.977 150.883L156 155H152V151H151.979C151.939 149.652 151.846 148.318 151.7 147C148.987 122.505 128.218 103.455 103 103.455C75.938 103.455 53.9999 81.4035 53.9999 54.3415C53.9999 28.2383 33.6831 6.77523 7.99994 5.10464C6.93301 5.03524 5.85681 5 4.77243 5H-6.10352e-05L0 1Z" fill="white" />
+                  <path d="M157 0H-6.10352e-05L0 1H4.77243C34.1812 1 57.9999 24.9568 57.9999 54.3415C57.9999 79.2064 78.1592 99.455 103 99.455C131.745 99.455 155.145 122.338 155.977 150.883L156 155H157V0Z" fill="#E5E5E5" />
+               </svg>
+
                <CardHeader>
                   <h4 className="font-semibold tracking-wide">Experience</h4>
                </CardHeader>
