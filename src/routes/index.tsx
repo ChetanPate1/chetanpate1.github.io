c682cff63f93c4b74beb7dd9143d90b389afe12e
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-09-07 20:38:51 +0100

    home WIP

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import { createHashRouter } from "react-router-dom";
 import Root from "./Root";
 import Home from "@/pages/Home";
 import Sketches from "@/pages/Sketches";
+import Skills from "@/pages/Skills";
+import Experience from "@/pages/Experience";
 import ErrorPage from "@/pages/ErrorPage";
 
 const router = createHashRouter([
@@ -14,6 +16,14 @@ const router = createHashRouter([
       {
         path: "/",
         element: <Home />
+      },
+      {
+        path: "/skills",
+        element: <Skills />
+      },
+      {
+        path: "/experience",
+        element: <Experience />
       },
       {
         path: "/sketches",
