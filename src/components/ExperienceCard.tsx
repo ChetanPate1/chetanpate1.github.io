49620a107afa53ee9cb50ab86e2a160458a04019
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-09-13 18:50:48 +0100

    dark mode WIP

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -13,8 +13,7 @@ type Props = {
 
 const ExperienceCard = (props: Props) => {
    const { skills = [], time, type, companyLogoName, active, onClick } = props;
-   const colourClass = active ? 'bg-primary' : '';
-   const imageName = active ? 'white' : 'black';
+   const colourClass = active ? 'bg-primary dark:bg-primary' : '';
    const activeHeight = active ? 'min-h-56' : 'min-h-40';
 
    return (
@@ -24,16 +23,21 @@ const ExperienceCard = (props: Props) => {
          </p>
 
          <CardContent className="relative px-10 pt-24">
-            <svg className="absolute -right-[4px] -top-[4px] z-0" width="194" height="119" viewBox="0 0 194 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <svg className="absolute -right-[4px] -top-[4px] z-0 opacity-100 dark:opacity-0" width="194" height="119" viewBox="0 0 194 119" fill="none" xmlns="http://www.w3.org/2000/svg">
                <path fillRule="evenodd" clipRule="evenodd" d="M0.5 0C19.5538 0 35 15.4462 35 34.5C35 53.5538 50.4462 69 69.5 69H144C171.614 69 194 91.3858 194 119V0H0.5Z" fill="#E5E5E5" />
                <path d="M194 119C194 112.032 192.575 105.398 190 99.3713C182.37 81.5142 164.647 69 144 69H69.5C50.4462 69 35 53.5538 35 34.5C35 21.2768 27.5608 9.7912 16.6393 4C11.8235 1.44641 6.33066 0 0.5 0V4C17.3447 4 31 17.6553 31 34.5C31 55.763 48.237 73 69.5 73H144C169.405 73 190 93.5949 190 119H194Z" fill="white" />
             </svg>
 
+            <svg className="absolute -right-[4px] -top-[4px] z-0 opacity-0 dark:opacity-100" width="194" height="119" viewBox="0 0 194 119" fill="none" xmlns="http://www.w3.org/2000/svg">
+               <path fillRule="evenodd" clipRule="evenodd" d="M0.5 0C19.5538 0 35 15.4462 35 34.5C35 53.5538 50.4462 69 69.5 69H144C171.614 69 194 91.3858 194 119V0H0.5Z" fill="black" />
+               <path d="M194 119C194 112.032 192.575 105.398 190 99.3713C182.37 81.5142 164.647 69 144 69H69.5C50.4462 69 35 53.5538 35 34.5C35 21.2768 27.5608 9.7912 16.6393 4C11.8235 1.44641 6.33066 0 0.5 0V4C17.3447 4 31 17.6553 31 34.5C31 55.763 48.237 73 69.5 73H144C169.405 73 190 93.5949 190 119H194Z" fill="#262626" />
+            </svg>
+
             <div className={`${activeHeight} transition-all duration-500`}>
-               <img className="absolute top-10 left-10" src={`./experience/${companyLogoName}-${imageName}.png`} />
+               <img className={`absolute top-10 left-10 ${active ? 'invert' : ''} dark:invert`} src={`./experience/${companyLogoName}-black.png`} />
 
                <div className="flex flex-row flex-wrap gap-2.5 mt-7">
-                  {skills.map((skill, i) => <Badge key={i} variant={active ? 'secondary' : 'default'}>{skill}</Badge>)}
+                  {skills.map((skill, i) => <Badge className="dark:border-transparent" key={i} variant={active ? 'secondary' : 'default'}>{skill}</Badge>)}
                </div>
             </div>
 
