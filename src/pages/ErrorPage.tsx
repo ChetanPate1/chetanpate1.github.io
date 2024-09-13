b62864ec76c74259665b3f3dd1dd249336220c42
Chetan Patel <chetanpate1@hotmail.co.uk>
2024-09-13 14:52:27 +0100

    type error fixes

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,18 +1,27 @@
 // Third Party
 import { useRouteError } from "react-router-dom";
 
-type ErrorType = {
+interface RouteError {
+   data: string;
+   error: {
+      columnNumber: number;
+      fileName: string;
+      lineNumber: number;
+      message: string;
+      stack: string;
+   };
+   internal: boolean;
+   status: number;
    statusText: string;
-   message: string;
-};
+}
 
 const ErrorPage = () => {
-   const error: ErrorType = useRouteError();
+   const error = useRouteError() as RouteError;
 
    return (
       <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-5 justify-items-center gap-4">
          <h1>404</h1>
-         <p>{error.statusText || error.message}</p>
+         <p>{error.statusText || error.error.message}</p>
       </div>
    );
 };
