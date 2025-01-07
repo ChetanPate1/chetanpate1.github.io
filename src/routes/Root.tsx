ac0ce8c0eaba1f1a4b59d92247df1a457707f7e5
Chetan Patel <chetanpate1@hotmail.co.uk>
2025-01-07 15:41:57 +0000

    dark mode logo fix

--- src/routes/Root.tsx
+++ src/routes/Root.tsx
@@ -9,7 +9,7 @@ const Root = () => {
    return (
       <div className="bg-neutral-200 dark:bg-neutral-950 h-full min-h-screen">
          <NavLink to="/">
-            <Logo size={45} className="fixed top-10 left-10 z-50" />
+            <Logo size={45} className="fixed top-10 left-10 z-50 dark:invert" />
          </NavLink>
          <DarkModeToggle />
          <Navbar />
